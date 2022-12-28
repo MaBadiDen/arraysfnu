2035b749b664915e95233e35755cfa56741e37fc
truemabadi <kaba4ok.den@gmail.com>
2022-12-29 00:50:19 +0300

    ready

--- src/StringListImpl.java
+++ src/StringListImpl.java
@@ -1,2 +1,165 @@
-package PACKAGE_NAME;public class StringListImpl {
+import exceptions.ElementNotFoundException;
+import exceptions.InvalidIndexException;
+import exceptions.NullItemException;
+import exceptions.StorageIsFullException;
+
+import java.util.Arrays;
+
+public class StringListImpl implements StringList {
+
+    private final String[] storage;
+    private int size;
+
+    public StringListImpl() {
+        storage = new String[10];
+    }
+
+    public StringListImpl(int initSize) {
+        storage = new String[initSize];
+    }
+
+
+    @Override
+    public String add(String item) {
+        validateSize();
+        validateNull(item);
+        storage[size++] = item;
+        return item;
+    }
+
+    @Override
+    public String add(int index, String item) {
+        validateSize();
+        validateNull(item);
+        validateIndex(index);
+
+        if(index == size) {
+            storage[size++] = item;
+
+        } else {
+            System.arraycopy(storage, index, storage, index + 1, size - index);
+            storage[index] = item;
+            size++;
+        }
+
+        return item;
+    }
+
+    @Override
+    public String set(int index, String item) {
+        validateIndex(index);
+        validateNull(item);
+        storage[index] = item;
+        return storage[index];
+    }
+
+    @Override
+    public String remove(String item) {
+        validateNull(item);
+        size--;
+
+        int index = indexOf(item);
+
+        if(index == -1) {
+            throw new ElementNotFoundException();
+        }
+
+        if(index != size) {
+            System.arraycopy(storage, index + 1, storage, index,  size - index);
+        }
+        size--;
+
+
+        return item;
+    }
+
+    @Override
+    public String remove(int index) {
+        validateIndex(index);
+
+        String item = storage[index];
+
+        if(index != size) {
+            System.arraycopy(storage, index + 1, storage, index,  size - index);
+        }
+        size--;
+
+
+        return item;
+    }
+
+    @Override
+    public boolean contains(String item) {
+        return indexOf(item) != -1;
+    }
+
+    @Override
+    public int indexOf(String item) {
+        for (int i = 0; i < size; i++) {
+            String string = storage[i];
+            if(string.equals(item)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(String item) {
+        for (int i = size - 1; i >= 0; i--) {
+            String string = storage[i];
+            if(string.equals(item)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public String get(int index) {
+        validateIndex(index);
+        return storage[index];
+    }
+
+    @Override
+    public boolean equals(StringList otherList) {
+        return Arrays.equals(this.toArray(), otherList.toArray());
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void clear() {
+
+
+    }
+
+    @Override
+    public String[] toArray() {
+        return Arrays.copyOf(storage, size);
+    }
+
+    private void validateNull(String item) {
+        if(item == null) {
+            throw new NullItemException();
+        }
+    }
+    private void validateSize() {
+        if(size == storage.length) {
+            throw new StorageIsFullException();
+        }
+    }
+    private void validateIndex(int index) {
+        if(index < 0 || index > size) {
+            throw new InvalidIndexException();
+        }
+    }
 }
